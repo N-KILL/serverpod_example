c7e224e2835fc4a2ebf52d5190a75e6b0ab7bf86
N-KILL <club_dog2@hotmail.com>
2023-05-19 12:24:22 -0300

    Tukis

--- testing/testing_server/lib/src/endpoints/example_endpoint.dart
+++ testing/testing_server/lib/src/endpoints/example_endpoint.dart
@@ -2,7 +2,7 @@ import 'package:serverpod/serverpod.dart';
 
 class ExampleEndpoint extends Endpoint {
   Future<String> hello(Session session, String name) async {
-    return "tuki";
+    return "Hola, $name";
   }
 }
 
